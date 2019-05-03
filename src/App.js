42679fd4797bfa3ca22729dd38143b468575e3a0
Melis uulu Nurjigit <nurjigit.melis.uulu@gmail.com>
2019-05-03 14:43:15 +0600

    installed router, axios, redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <Route path="/" exact component={} />
+        <Route path="/" exact component={} />
+        <Route path="/" exact component={} />
       </div>
     );
   }

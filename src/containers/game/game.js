2eb0f9889edfd32fa2d1962c6c70d8009691c65a
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-05-17 10:34:15 +0600

    fix jump func

--- src/containers/game/game.js
+++ src/containers/game/game.js
@@ -3,38 +3,42 @@ import classes from "./Game.module.css";
 
 class Game extends Component {
   state = {
-    jumping: false,
+    jumping: true
   };
 
   jump = () => {
-    this.setState({
-      jumping: !this.state.jumping
-    })
-    let jumpingInterval = setTimeout(() => {
+    if (this.state.jumping) {
       this.setState({
-        jumping: !this.state.jumping
-      })
-      clearTimeout(jumpingInterval);
-    }, 1400)
-  }
+        jumping: false
+      });
+      let jumpingInterval = setTimeout(() => {
+        this.setState({
+          jumping: !this.state.jumping
+        });
+        clearTimeout(jumpingInterval);
+      }, 1400);
+    }
+  };
 
   render() {
     let playerClasses = [classes.dino];
     if (this.state.jumping) {
-      playerClasses = [classes.dino, classes.jump].join(" ");
-    } else {
       playerClasses = [classes.dino];
+    } else {
+      playerClasses = [classes.dino, classes.jump].join(" ");
     }
-    let player = <div className={playerClasses}></div>
+    let player = <div className={playerClasses} />;
     return (
-      <div className={classes.container} >
+      <div className={classes.container}>
         <div className={classes.game}>
           {player}
           <div className={classes.object} />
           <div className={classes.platform} />
         </div>
         <div className={classes.controls}>
-          <button className={classes.jump} onClick={this.jump}>Jump</button>
+          <button className={classes.jump} onClick={this.jump}>
+            Jump
+          </button>
         </div>
       </div>
     );

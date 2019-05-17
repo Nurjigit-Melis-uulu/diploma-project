41a38b9035eddce1ad0d2173a7cc1fca35c1b8cf
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-05-17 15:24:04 +0600

    add function for check

--- src/containers/game/game.js
+++ src/containers/game/game.js
@@ -3,11 +3,24 @@ import classes from "./Game.module.css";
 
 class Game extends Component {
   state = {
-    jumping: true
+    jumping: true,
+    object: null,
+    player: null,
+    platform: null,
+    game: null
   };
 
-  jump = () => {
+  jump = event => {
+    let object =
+      event.target.parentElement.parentElement.children[0].children[1];
+    let player =
+      event.target.parentElement.parentElement.children[0].children[0];
+    let platform =
+      event.target.parentElement.parentElement.children[0].children[3];
+    let game = event.target.parentElement.parentElement.children[0];
+
     if (this.state.jumping) {
+      object.style.transform = "translate(-1020px)";
       this.setState({
         jumping: false
       });
@@ -18,6 +31,43 @@ class Game extends Component {
         clearTimeout(jumpingInterval);
       }, 1400);
     }
+
+    console.log(object, player, platform, game);
+    this.moving(object, player, platform, game);
+  };
+
+  moving = (object, player, platform, game) => {
+    let objectPosition = object.getBoundingClientRect();
+    let playerPosition = player.getBoundingClientRect();
+    let platformPosition = platform.getBoundingClientRect();
+    let gamePosition = game.getBoundingClientRect();
+    let stop = false;
+    let move = null;
+
+    if (
+      objectPosition.x < playerPosition.width + playerPosition.x &&
+      objectPosition.x > playerPosition.x
+    ) {
+      if (
+        playerPosition.y + playerPosition.height === platformPosition.y ||
+        playerPosition.y + playerPosition.height > platformPosition.y + 20
+      ) {
+        console.log("objectsMove interval have cleared in condition");
+        stop = true;
+      }
+    }
+
+    if (objectPosition.x <= gamePosition.x) {
+      console.log("objectsMove interval have cleared");
+      stop = true;
+    }
+
+    stop
+      ? (move = null)
+      : (move = setTimeout(() => {
+          clearTimeout(move);
+          this.moving(object, player, platform, game);
+        }, 1000));
   };
 
   render() {
@@ -28,16 +78,18 @@ class Game extends Component {
       playerClasses = [classes.dino, classes.jump].join(" ");
     }
     let player = <div className={playerClasses} />;
+
     return (
       <div className={classes.container}>
         <div className={classes.game}>
           {player}
           <div className={classes.object} />
+          <div className={classes.screen} />
           <div className={classes.platform} />
         </div>
         <div className={classes.controls}>
           <button className={classes.jump} onClick={this.jump}>
-            Jump
+            JUMP
           </button>
         </div>
       </div>

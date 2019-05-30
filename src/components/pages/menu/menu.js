6b25a35b825b46f532e3da807063f8450914e491
Nurjigit <45354207+Nurjigit-Melis-uulu@users.noreply.github.com>
2019-05-31 00:18:26 +0600

    Stylized menu

--- src/components/pages/menu/menu.js
+++ src/components/pages/menu/menu.js
@@ -6,11 +6,13 @@ import classes from "./Menu.module.css";
 function Menu(props) {
   return (
     <div className={classes.Menu}>
-      <h1>Kyz-kuumay</h1>
-      <small>The Kyrgyz national game</small>
-      <div className={classes.menuControl}>
-        <NavLink to="/LevelsPage">Start game</NavLink>
-        <NavLink to="/AboutGame">About the game</NavLink>
+      <div className={classes.content}>
+        <h1>Kyz-kuumay</h1>
+        <span>The Kyrgyz national game</span>
+        <div className={classes.menuControl}>
+          <NavLink to="/LevelsPage">Start game</NavLink>
+          <NavLink to="/AboutGame">About the game</NavLink>
+        </div>
       </div>
     </div>
   );
